6be00f031ead7bc6a331cd2121843d13fd72f9d8
vossler <joshvosslerwork@gmail.com>
2025-11-03 02:05:28 -0500

    Imported some frontend code to test React.js routing

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,35 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { getMovieDetails, getPopularMovies } from "../../utils/api";
+import NavBar from "../layout/NavBar/NavBar"
 
 function HomePage() {
-  const [movies, setMovies] = useState([]);
-  const [details, setDetails] = useState([]);
-
-  useEffect(() => {
-    getPopularMovies()
-      .then(data => setMovies(data.results));
-  }, []);
-
-  useEffect(() => {
-    Promise.all(
-      movies.map(movie => getMovieDetails(movie.id))
-    )
-      .then(allDetails => setDetails(allDetails));
-  }, [movies]);
-
   return (
-    <div>
-      <h1>Popular Movies</h1>
-      <ul>
-        {movies.map((m, index) => (
-          <li key={m.id}>
-            {details[index]?.poster_path?.[0] && (
-              <img src={`https://image.tmdb.org/t/p/w500${details[index].poster_path}`}/>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <NavBar />
   );
 }
 
