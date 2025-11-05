f84d8b92076c47b3675d754c2130b89aac51b435
vossler <joshvosslerwork@gmail.com>
2025-11-05 01:46:49 -0500

    Prepping backend for user authentication with mongodb, currently User.js creates a schema in the mongodb Watchr database and auth.js creates api routes for registration/login. Not currently being implemented in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import tmdbRoutes from "./routes/tmdb.js";
+import authRoutes from "./routes/auth.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use("/routes/tmdb", tmdbRoutes);
 
-app.listen(5000);
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
+
+app.use("/routes/tmdb", tmdbRoutes);
+app.use("/routes/auth", authRoutes);
+
+app.listen(5000, () => console.log("Server running at port 5000"));
---
+++ backend/routes/auth.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/User.js";
+dotenv.config({path: "../.env"});
+
+const router = express.Router();
+
+router.post("/register", async(req,res) => {
+    const { usernam, password } = req.body;
+
+    try {
+        let user = await User.findOne({ username });
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        user = new User({ username, password});
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: { id: user.id }
+        };
+
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, 
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        let user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.jwtSecret, { expiresIn: 3600 }, 
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+export default router;
