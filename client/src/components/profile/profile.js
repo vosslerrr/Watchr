7f4f5a65838f5cd1995a53d2ffa6b695f732a96b
vossler <joshvosslerwork@gmail.com>
2025-11-08 17:30:11 -0500

    Finished UserInfo.js placheholder layout, updated the upcomingMovies api call for more specific filters, updated App.js to inclkude the Profile page

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,9 +1,11 @@
 import NavBar from "../layout/NavBar/NavBar"
+import UserInfo from "../layout/UserInfo/UserInfo";
 
 function Profile() {
   return (
     <div>
       <NavBar />
+      <UserInfo />
     </div>
   );
 }
