7920fe0297f4c73cd669f9d183468a4aecf28a8a
vossler <joshvosslerwork@gmail.com>
2025-11-09 00:29:20 -0500

    Added Review.js which creates a new mongoose schema, also preparing to merge code into main

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,11 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatarURL: String,
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
 
 const User = mongoose.model("User", UserSchema);
--- client/src/components/layout/UserReviews/UserReviews.js
+++ client/src/components/layout/UserReviews/UserReviews.js
@@ -1,9 +1,12 @@
-import './userReviews.css'; 
+import "./userReviews.css"; 
 
 function UserReviews(){
     return(
         <div className="UserReviews">
-
+            <div className="Header">
+                <span id="reviewHeader">Recent Reviews</span>
+                <a id="seeAll" href="/">See All</a>
+            </div>
         </div>
     );
 }
