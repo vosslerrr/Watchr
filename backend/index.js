b60eb969ebdd6ac8a9702966d389b30958226cd4
vossler <joshvosslerwork@gmail.com>
2025-11-13 23:31:41 -0500

    AWS EC2 integration test

--- backend/index.js
+++ backend/index.js
@@ -18,4 +18,4 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/routes/tmdb", tmdbRoutes);
 app.use("/routes/auth", authRoutes);
 
-app.listen(5000, '0.0.0.0', () => console.log("Server running at port 5000"));
+app.listen(5000, "0.0.0.0", () => console.log("Server running at port 5000"));
