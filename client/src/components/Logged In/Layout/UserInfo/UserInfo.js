6d152f58d5c71ba8dc3df396a45fb1e4bfb8ffc6
vossler <joshvosslerwork@gmail.com>
2025-12-02 13:31:02 -0500

    Finished displaying the correct average rating for the user on the profile page

--- client/src/components/Logged In/Layout/UserInfo/UserInfo.js
+++ client/src/components/Logged In/Layout/UserInfo/UserInfo.js
@@ -1,12 +1,14 @@
 import "./UserInfo.css"
 import React, { useState, useEffect } from 'react';
-import { getUserDetails } from "../../../../utils/api";
+import { getUserDetails, getUserReviews } from "../../../../utils/api";
 import { useParams } from "react-router-dom";
 
 function UserInfo(){
     const { username } = useParams();
     const [numFollowers, setNumFollowers] = useState('');
     const [numFollowing, setNumFollowing] = useState('');
+    const [ratings, setRatings] = useState(0);
+    const [count, setCount] = useState(0);
     
     useEffect(() => {
         async function load(){
@@ -14,17 +16,28 @@ function UserInfo(){
 
             setNumFollowers(res.followers.length);
             setNumFollowing(res.following.length);
+
+            const reviews = await getUserReviews(username);
+
+            var totalRating = 0;
+            reviews.map(review => {
+                totalRating += review.rating;
+            })
+
+            setRatings(totalRating);
+            setCount(reviews.length);
         }
         
         load();
     }, []);
+
 
     return(
         <div className="UserInfo">
             <img id="avatar" src="/user-default.png" />
             <span id="username">{username}</span>
             <span className="avgRating">
-                <span id="avgNum">9.5</span>
+                <span id="avgNum">{(ratings/count).toFixed(2)}</span>
                 <img src="/rating-star.png"></img>
                 <span id="avgText">Avg Rating</span>   
             </span>
