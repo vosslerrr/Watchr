9a9b85c583a0f46f0022d3fba0761495e98f65da
vossler <joshvosslerwork@gmail.com>
2025-12-01 22:04:01 -0500

    Added the poster and movie title to the user reviews, still needs to be formatted properly

--- client/src/components/Logged In/Layout/UserReviews/UserReviews.js
+++ client/src/components/Logged In/Layout/UserReviews/UserReviews.js
@@ -1,12 +1,12 @@
 import "./userReviews.css";
-import { getUserReviews } from '../../../../utils/api' 
+import { getUserReviews, getMovieDetails } from '../../../../utils/api' 
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 
 function UserReviews(){
     const { username } = useParams();
     const [userReviews, setUserReviews] = useState([]);
-    const [movieID, setMovieID] = useState([]);
+    const [details, setDetails] = useState([]);
 
     useEffect(() => {
             async function load(){
@@ -18,19 +18,33 @@ function UserReviews(){
             load();
     }, []);
 
+    useEffect(() => {
+        if(userReviews != null){
+            Promise.all(
+                userReviews.map(review => getMovieDetails(review.movie_id))
+            )
+                .then(allIDs => setDetails(allIDs));
+        }
+    }, [userReviews]);
+
     return(
         <div className="UserReviews">
             <div className="Header">
                 <span id="reviewHeader">Recent Reviews</span>
                 <a id="seeAll" href="/">See All</a>
             </div>
+
             <div className="Reviews">
                 {userReviews.map((review, index) => {
                     return(
                         review ? (
                             <div id="reviewLayout">
-                                <div id="reviewPara">{review.reviewPara}</div>
-                                <div id="rating">{review.rating}</div>
+                                <div id="poster"><img src={`https://image.tmdb.org/t/p/w500${details[index]?.poster_path}`}></img></div>
+                                <div id="reviewContent">
+                                    <span id="movieTitle">{details[index]?.original_title} ({details[index]?.release_date.slice(0,4)})</span>
+                                    <span id="reviewPara">{review.reviewPara}</span>
+                                    <span id="rating">{review.rating}</span>
+                                </div>
                             </div>
                         ) : null (
                             <div id="noReviews">You have no reviews</div>
@@ -38,6 +52,7 @@ function UserReviews(){
                     );
                 })}
             </div>
+
         </div>
     );
 }
